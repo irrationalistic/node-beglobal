9db9b18ebc4991ec412ec831d6780abfb7c8b17c
Robert Onodi <usdh_1989@yahoo.com>
2014-06-25 15:49:01 +0300

    translation input html format

--- test/translation_test.js
+++ test/translation_test.js
@@ -1,12 +1,29 @@
 var chai = require('chai');
 var should = chai.should();
-var BeglobalAPI = require('../lib/beglobal').BeglobalAPI;
+var LanguageCloudAPI = require('../lib/languagecloud').LanguageCloudAPI;
 var configs = require('../lib/config.js');
-var beglobal = new BeglobalAPI(configs);
+var lc = new LanguageCloudAPI(configs);
 
 describe('Translations', function() {
   it('should get a translation', function(done) {
-    beglobal.translations.translate({text: 'Hello', from: 'eng', to: 'fra'}, function(err, results) {
+    lc.translations.translate({text: 'Hello', from: 'eng', to: 'fra'}, function(err, results) {
+      if (err) throw err;
+
+      should.not.exist(err);
+      should.exist(results);
+      done();
+    });
+  });
+
+  it('should get a translation in html format using `encodeURIComponent`', function(done) {
+    lc.translations.translate({
+      text: '<html><head></head><body><h1>Hello, how are you?</h1></body></html>',
+      from: 'eng',
+      to: 'fra',
+      inputFormat: 'html'
+    }, function(err, results) {
+      if (err) throw err;
+
       should.not.exist(err);
       should.exist(results);
       done();
